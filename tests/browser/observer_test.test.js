a69560602036a7dc05365fbd770122810ed1b7cb
Vjacheslav Trushkin <cyberalien@gmail.com>
2017-11-09 17:13:45 +0200

    Minor typo in test

--- tests/browser/observer_test.test.js
+++ tests/browser/observer_test.test.js
@@ -43,7 +43,7 @@
                 initQueue: [],
                 nodesAdded: function(nodes) {
                     called = true;
-                },
+                }
             };
 
             // Load observer
