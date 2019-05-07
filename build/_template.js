c2bc8e649aac391f860df2ae030144ef93bdab03
Vjacheslav Trushkin <cyberalien@gmail.com>
2019-05-07 10:51:03 +0300

    Rename GitHub organization to iconify, update dependencies

--- build/_template.js
+++ build/_template.js
@@ -2,7 +2,7 @@
  * (c) Vjacheslav Trushkin <cyberalien@gmail.com>
  *
  * For the full copyright and license information, please view the license.txt or license.gpl.txt
- * files at https://github.com/iconify-design/iconify
+ * files at https://github.com/iconify/iconify
  *
  * Licensed under Apache 2.0 or GPL 2.0 at your option.
  * If derivative product is not compatible with one of licenses, you can pick one of licenses.
