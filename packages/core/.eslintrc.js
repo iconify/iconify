72dc86c2e3c9591df56afb457e5e0021d511547b
Vjacheslav Trushkin <cyberalien@gmail.com>
2020-09-03 10:36:15 +0300

    Update dependencies

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -3,28 +3,28 @@ module.exports = {
 		browser: true,
 		es6: true,
 		node: true,
-		mocha: true
+		mocha: true,
 	},
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
 	globals: {
 		Atomics: 'readonly',
-		SharedArrayBuffer: 'readonly'
+		SharedArrayBuffer: 'readonly',
 	},
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 2018,
 		sourceType: 'module',
-		project: './tsconfig-base.json'
+		project: __dirname + '/tsconfig-base.json',
 	},
 	plugins: ['@typescript-eslint'],
 	rules: {
 		'no-mixed-spaces-and-tabs': ['off'],
 		'no-unused-vars': ['off'],
-		'@typescript-eslint/no-unused-vars-experimental': ['error']
+		'@typescript-eslint/no-unused-vars-experimental': ['error'],
 	},
 	overrides: [
 		{
-			files: ['src/**/*.ts', 'tests/**/*.ts']
-		}
-	]
+			files: ['src/**/*.ts', 'tests/**/*.ts'],
+		},
+	],
 };
