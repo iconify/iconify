818f78eda46c3c604cfbc49dc0a680a4bb1e458c
Vjacheslav Trushkin <cyberalien@gmail.com>
2021-09-28 09:40:55 +0300

    Add Svelte demo with Vite, add SveteKit demo

---
+++ packages/svelte-demo-vite/src/App.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+  import Icon from '@iconify/svelte';
+</script>
+
+<main>
+  <div class="logo">
+    <Icon icon="logos:svelte-icon" style="display: block; margin: 0 auto;" />
+  </div>
+
+  <h1><Icon icon="gg:hello" title="Hello" /> <Icon icon="logos:typescript" title="TypeScript" /> <Icon icon="emojione:heavy-heart-exclamation" title="!" /></h1>
+
+  <Counter />
+
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev <Icon icon="ci:external-link" inline={true} /></a> to learn how to build <Icon icon="logos:svelte" inline={true} />
+    apps.
+  </p>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit <Icon icon="ci:external-link" inline={true} /></a> for
+    the officially supported framework, also powered by Vite!
+  </p>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  .logo {
+    font-size: 16rem;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  :global(h1 svg) {
+    vertical-align: -0.125em;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
---
+++ packages/sveltekit-demo/src/lib/header/Header.svelte
@@ -0,0 +1,124 @@
+<script lang="ts">
+	import Icon, { addIcon } from '@iconify/svelte';
+	import { page } from '$app/stores';
+
+	addIcon('corner-left', {
+		body: '<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" fill="currentColor" />',
+		width: 2,
+		height: 3
+	});
+	addIcon('corner-right', {
+		body: '<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" fill="currentColor" />',
+		width: 2,
+		height: 3
+	});
+</script>
+
+<header>
+	<div class="corner">
+		<a href="https://kit.svelte.dev">
+			<Icon icon="logos:svelte-icon" title="SvekteKit" />
+		</a>
+	</div>
+
+	<nav>
+		<Icon icon="corner-left" height="auto" />
+		<ul>
+			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
+			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
+		</ul>
+		<Icon icon="corner-right" height="auto" />
+	</nav>
+
+	<div class="corner">
+		<a href="https://github.com/iconify/iconify/tree/master/packages/svelte">
+			<Icon icon="line-md:iconify2" title="Iconify" />
+		</a>
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner :global(svg) {
+		font-size: 2em;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	nav :global(svg) {
+		width: 2em;
+		height: 3em;
+		display: block;
+		color: var(--background);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li.active::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--accent-color);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 1em;
+		color: var(--heading-color);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--accent-color);
+	}
+</style>
