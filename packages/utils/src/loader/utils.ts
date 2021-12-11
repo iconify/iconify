0cdd57c7c77edc777855fd65d7150167db18a886
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-12-11 21:36:36 +0100

    chore: add export to utils index

--- packages/utils/src/loader/utils.ts
+++ packages/utils/src/loader/utils.ts
@@ -1,43 +1,44 @@
-import { installPackage } from '@antfu/install-pkg'
-import { sleep } from '@antfu/utils'
+import { installPackage } from '@antfu/install-pkg';
+import { sleep } from '@antfu/utils';
 
 export function camelize(str: string): string {
-	return str.replace(/-([a-z0-9])/g, g => g[1].toUpperCase())
+	return str.replace(/-([a-z0-9])/g, g => g[1].toUpperCase());
 }
 
 export function pascalize(str: string): string {
-	const camel = camelize(str)
-	return camel[0].toUpperCase() + camel.slice(1)
+	const camel = camelize(str);
+	return camel[0].toUpperCase() + camel.slice(1);
 }
 
 export function camelToKebab(key: string): string {
 	const result = key
 		.replace(/:/g, '-')
 		.replace(/([A-Z])/g, ' $1')
-		.trim()
-	return result.split(/\s+/g).join('-').toLowerCase()
+		.trim();
+	return result.split(/\s+/g).join('-').toLowerCase();
 }
 
-const warnned = new Set<string>()
+const warnned = new Set<string>();
 
 export function warnOnce(msg: string): void {
 	if (!warnned.has(msg)) {
-		warnned.add(msg)
-		console.warn(`[@iconify-loader] ${msg}`)
+		warnned.add(msg);
+		console.warn(`[@iconify-loader] ${msg}`);
 	}
 }
 
 
-let pending: Promise<void> | undefined
-const tasks: Record<string, Promise<void> | undefined> = {}
+let pending: Promise<void> | undefined;
+const tasks: Record<string, Promise<void> | undefined> = {};
 
 export async function tryInstallPkg(name: string): Promise<void | undefined> {
-	if (pending)
-		await pending
+	if (pending) {
+		await pending;
+	}
 
 	if (!tasks[name]) {
 		// eslint-disable-next-line no-console
-		console.log(`Installing ${name}...`)
+		console.log(`Installing ${name}...`);
 		tasks[name] = pending = installPackage(name, { dev: true, preferOffline: true })
 			.then(() => sleep(300))
 			// eslint-disable-next-line
@@ -47,9 +48,9 @@ export async function tryInstallPkg(name: string): Promise<void | undefined> {
 			})
 			.finally(() => {
 				pending = undefined
-			})
+			});
 	}
 
 	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-	return tasks[name]!
+	return tasks[name]!;
 }
