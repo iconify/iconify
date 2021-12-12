c119726e1a82c5fdfa79bf7a10ebde5c737708e3
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-12-12 11:22:44 +0100

    chore: add `kolorist`

--- packages/utils/src/loader/utils.ts
+++ packages/utils/src/loader/utils.ts
@@ -1,5 +1,6 @@
 import { installPackage } from '@antfu/install-pkg';
 import { sleep } from '@antfu/utils';
+import { cyan, yellow } from 'kolorist'
 
 export function camelize(str: string): string {
 	return str.replace(/-([a-z0-9])/g, g => g[1].toUpperCase());
@@ -23,7 +24,7 @@ const warnned = new Set<string>();
 export function warnOnce(msg: string): void {
 	if (!warnned.has(msg)) {
 		warnned.add(msg);
-		console.warn(`[@iconify-loader] ${msg}`);
+		console.warn(yellow(`[@iconify-loader] ${msg}`))
 	}
 }
 
@@ -38,7 +39,7 @@ export async function tryInstallPkg(name: string): Promise<void | undefined> {
 
 	if (!tasks[name]) {
 		// eslint-disable-next-line no-console
-		console.log(`Installing ${name}...`);
+		console.log(cyan(`Installing ${name}...`))
 		tasks[name] = pending = installPackage(name, { dev: true, preferOffline: true })
 			.then(() => sleep(300))
 			// eslint-disable-next-line
