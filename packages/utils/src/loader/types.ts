30c4ddf3f1dc01566a4cbee946c03b080cbab932
Vjacheslav Trushkin <cyberalien@gmail.com>
2022-01-09 23:23:02 +0200

    Add annotations, clean up getCustomIcon and test loading bad icon

--- packages/utils/src/loader/types.ts
+++ packages/utils/src/loader/types.ts
@@ -1,10 +1,21 @@
 import type { Awaitable } from '@antfu/utils';
 
+/**
+ * Custom icon loader, used by getCustomIcon()
+ */
 export type CustomIconLoader = (name: string) => Awaitable<string | undefined>;
+
+/**
+ * List of icons as object. Key is icon name, value is icon data or callback (can be async) to get icon data
+ */
 export type InlineCollection = Record<
 	string,
 	string | (() => Awaitable<string | undefined>)
 >;
+
+/**
+ * Collection of custom icons. Key is collection name, value is loader or InlineCollection object
+ */
 export type CustomCollections = Record<
 	string,
 	CustomIconLoader | InlineCollection
