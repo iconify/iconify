9c0ba713556d0f30dc051908c3fc622380db3825
Vjacheslav Trushkin <cyberalien@gmail.com>
2022-01-09 22:44:48 +0200

    Move fixtures in utils to fixtures directory

--- packages/utils/tests/get-custom-icon-test.ts
+++ packages/utils/tests/get-custom-icon-test.ts
@@ -1,16 +1,23 @@
 import { getCustomIcon } from '../lib';
 
-const svg = '<svg viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg"><circle cx="60" cy="60" r="50"/></svg>'
+const svg =
+	'<svg viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg"><circle cx="60" cy="60" r="50"/></svg>';
 
 describe('Testing getCustomIcon', () => {
-	test('CustomIconLoader', async() => {
-		const result = await getCustomIcon(() => svg, 'a', 'b')
+	test('CustomIconLoader', async () => {
+		const result = await getCustomIcon(() => svg, 'a', 'b');
 		expect(svg).toEqual(result);
 	});
-	test('CustomIconLoader with transform', async() => {
-		const result = await getCustomIcon(() => svg, 'a', 'b', (icon) => {
-			return icon.replace('<svg ', '<svg width="1em" height="1em" ')
-		})
+
+	test('CustomIconLoader with transform', async () => {
+		const result = await getCustomIcon(
+			() => svg,
+			'a',
+			'b',
+			(icon) => {
+				return icon.replace('<svg ', '<svg width="1em" height="1em" ');
+			}
+		);
 		expect(result && result.indexOf('width="1em"') > -1).toBeTruthy();
 		expect(result && result.indexOf('height="1em"') > -1).toBeTruthy();
 	});
