c1fb9f8e6caa8f1860e844cffd72b8953180ad9d
Vjacheslav Trushkin <cyberalien@gmail.com>
2022-01-09 22:05:18 +0200

    Move reusable string functions to separate file

--- packages/utils/src/loader/loaders.ts
+++ packages/utils/src/loader/loaders.ts
@@ -1,19 +1,24 @@
 import type { Awaitable } from '@antfu/utils';
 import { existsSync, promises as fs } from 'fs';
 import type { CustomIconLoader } from './types';
-import { camelize, pascalize } from './utils';
+import { camelize, pascalize } from '../misc/strings';
 
-export function FileSystemIconLoader(dir: string, transform?: (svg: string) => Awaitable<string>): CustomIconLoader {
-	return async(name) => {
+export function FileSystemIconLoader(
+	dir: string,
+	transform?: (svg: string) => Awaitable<string>
+): CustomIconLoader {
+	return async (name) => {
 		const paths = [
 			`${dir}/${name}.svg`,
 			`${dir}/${camelize(name)}.svg`,
-			`${dir}/${pascalize(name)}.svg`
+			`${dir}/${pascalize(name)}.svg`,
 		];
 		for (const path of paths) {
 			if (existsSync(path)) {
 				const svg = await fs.readFile(path, 'utf-8');
-				return typeof transform === 'function' ? await transform(svg) : svg;
+				return typeof transform === 'function'
+					? await transform(svg)
+					: svg;
 			}
 		}
 	};
