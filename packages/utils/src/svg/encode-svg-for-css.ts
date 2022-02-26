e507252841113dc921e7483140e0d8ab3a212b25
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2022-02-26 17:32:17 +0100

    chore: svg on css requires svg and xlink namespaces

--- packages/utils/src/svg/encode-svg-for-css.ts
+++ packages/utils/src/svg/encode-svg-for-css.ts
@@ -1,10 +1,17 @@
 // https://bl.ocks.org/jennyknuth/222825e315d45a738ed9d6e04c7a88d0
 export function encodeSvgForCss(svg: string): string {
-	return svg.replace(/"/g, '\'')
+	let useSvg = svg.startsWith('<svg>') ? svg.replace('<svg>', '<svg >') : svg;
+	if (!useSvg.includes(' xmlns:xlink=') && useSvg.includes(' xlink:')) {
+		useSvg = useSvg.replace('<svg ', '<svg xmlns:xlink="http://www.w3.org/1999/xlink" ');
+	}
+	if (!useSvg.includes(' xmlns=')) {
+		useSvg = useSvg.replace('<svg ', '<svg xmlns="http://www.w3.org/2000/svg" ');
+	}
+	return useSvg.replace(/"/g, '\'')
 		.replace(/%/g, '%25')
 		.replace(/#/g, '%23')
 		.replace(/{/g, '%7B')
 		.replace(/}/g, '%7D')
 		.replace(/</g, '%3C')
-		.replace(/>/g, '%3E')
+		.replace(/>/g, '%3E');
 }
