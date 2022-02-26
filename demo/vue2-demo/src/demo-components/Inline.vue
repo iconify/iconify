235c4e8f6c6cd1ff0f95176ff5dbbc2763d970ab
Vjacheslav Trushkin <cyberalien@gmail.com>
2022-02-26 18:38:27 +0200

    Fix conflicting IDs in Vue 2 demo

--- demo/vue2-demo/src/demo-components/Inline.vue
+++ demo/vue2-demo/src/demo-components/Inline.vue
@@ -4,12 +4,16 @@
 		<div>
 			Block icon (behaving like image):
 			<OfflineIcon icon="experiment2" />
-			<FullIcon icon="experiment2" />
+			<FullIcon icon="experiment2" id="inline-demo-block-full" />
 		</div>
 		<div>
 			Inline icon (behaving line text / icon font):
 			<OfflineIcon icon="experiment2" :inline="true" />
-			<FullIcon icon="experiment2" :inline="true" />
+			<FullIcon
+				icon="experiment2"
+				:inline="true"
+				id="inline-demo-inline-full"
+			/>
 		</div>
 	</section>
 </template>
