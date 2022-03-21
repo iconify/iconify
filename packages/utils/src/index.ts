bd935e49553c6f10b3a419ab2175f012d0608078
Vjacheslav Trushkin <cyberalien@gmail.com>
2022-03-21 18:54:25 +0200

    Add function to trim SVG to utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -40,6 +40,7 @@ export { iconToSVG } from './svg/build';
 export { replaceIDs } from './svg/id';
 export { calculateSize } from './svg/size';
 export { encodeSvgForCss } from './svg/encode-svg-for-css';
+export { trimSVG } from './svg/trim';
 
 // Colors
 export { colorKeywords } from './colors/keywords';
---
+++ packages/utils/src/svg/trim.ts
@@ -0,0 +1,17 @@
+/**
+ * Remove whitespace
+ */
+export function trimSVG(str: string): string {
+	return (
+		str
+			// Replace new line only after one of allowed characters that are not part of common attributes
+			.replace(/(["';{}}><])\s*\n\s*/g, '$1')
+			// Keep one space in case it is inside attribute
+			.replace(/\s*\n\s*/g, ' ')
+			// Trim attribute values
+			.replace(/\s+"/g, '"')
+			.replace(/="\s+/g, '="')
+			// Trim it
+			.trim()
+	);
+}
