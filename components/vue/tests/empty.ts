db7e0c2ffda95c42a1667f13fb6129b54f79b4a6
Vjacheslav Trushkin <cyberalien@gmail.com>
2022-06-19 22:25:00 +0300

    Update dependencies, make build work correctly with pnpm

--- components/vue/tests/empty.ts
+++ components/vue/tests/empty.ts
@@ -3,4 +3,4 @@ export const defaultIconResult =
 	'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" viewBox="0 0 16 16"></svg>';
 
 // Empty string: could be '<!---->' (jsdom) or '' (happy-dom), depending on testing envronment
-export const emptyString = '<!---->';
+export const emptyString = ''; //'<!---->';
