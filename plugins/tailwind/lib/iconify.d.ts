aa55348abc58394707f5d72f697a213085f55f1f
Vjacheslav Trushkin <cyberalien@gmail.com>
2023-01-11 23:05:50 +0200

    chore(tailwind): remove generated files

--- plugins/tailwind/lib/iconify.d.ts
+++
@@ -1,5 +0,0 @@
-import type { IconifyPluginOptions } from './options';
-/**
- * Get CSS rules for icon
- */
-export declare function getCSSRules(icons: string[] | string, options?: IconifyPluginOptions): Record<string, Record<string, string>>;
--- plugins/tailwind/lib/options.d.ts
+++
@@ -1,3 +0,0 @@
-import type { IconCSSIconSetOptions } from '@iconify/utils/lib/css/types';
-export interface IconifyPluginOptions extends IconCSSIconSetOptions {
-}
