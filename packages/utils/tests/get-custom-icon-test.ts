78f71407d5c7d46b522271b624d1c4932939392f
Vjacheslav Trushkin <cyberalien@gmail.com>
2023-02-14 12:15:30 +0200

    chore: publish update for utils with few more unit tests

--- packages/utils/tests/get-custom-icon-test.ts
+++ packages/utils/tests/get-custom-icon-test.ts
@@ -106,6 +106,18 @@ describe('Testing getCustomIcon', () => {
 	});
 
 	test('Scale custom icon', async () => {
+		const svg =
+			'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M139.61 35.5a12 12 0 0 0-17 0L58.93 98.81l-22.7-22.12a12 12 0 0 0-17 0L3.53 92.41a12 12 0 0 0 0 17l47.59 47.4a12.78 12.78 0 0 0 17.61 0l15.59-15.62L156.52 69a12.09 12.09 0 0 0 .09-17zm0 159.19a12 12 0 0 0-17 0l-63.68 63.72-22.7-22.1a12 12 0 0 0-17 0L3.53 252a12 12 0 0 0 0 17L51 316.5a12.77 12.77 0 0 0 17.6 0l15.7-15.69 72.2-72.22a12 12 0 0 0 .09-16.9zM64 368c-26.49 0-48.59 21.5-48.59 48S37.53 464 64 464a48 48 0 0 0 0-96zm432 16H208a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h288a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0-320H208a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h288a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16zm0 160H208a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h288a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z"/></svg>';
+
+		const options: IconifyLoaderOptions = {
+			scale: 2,
+		};
+		const result = await getCustomIcon(() => svg, 'a', 'b', options);
+		expect(result && result.indexOf(' width="2em"') > -1).toBeTruthy();
+		expect(result && result.indexOf(' height="2em"') > -1).toBeTruthy();
+	});
+
+	test('Scale custom icon with stroke-width', async () => {
 		const svg = `<?xml version="1.0" standalone="no"?>
 		<svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
 		  stroke="currentColor">
@@ -118,5 +130,17 @@ describe('Testing getCustomIcon', () => {
 		const result = await getCustomIcon(() => svg, 'a', 'b', options);
 		expect(result && result.indexOf(' width="1.2em"') > -1).toBeTruthy();
 		expect(result && result.indexOf(' height="1.2em"') > -1).toBeTruthy();
+	});
+
+	test('Disable scale for custom icon', async () => {
+		const svg =
+			'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M139.61 35.5a12 12 0 0 0-17 0L58.93 98.81l-22.7-22.12a12 12 0 0 0-17 0L3.53 92.41a12 12 0 0 0 0 17l47.59 47.4a12.78 12.78 0 0 0 17.61 0l15.59-15.62L156.52 69a12.09 12.09 0 0 0 .09-17zm0 159.19a12 12 0 0 0-17 0l-63.68 63.72-22.7-22.1a12 12 0 0 0-17 0L3.53 252a12 12 0 0 0 0 17L51 316.5a12.77 12.77 0 0 0 17.6 0l15.7-15.69 72.2-72.22a12 12 0 0 0 .09-16.9zM64 368c-26.49 0-48.59 21.5-48.59 48S37.53 464 64 464a48 48 0 0 0 0-96zm432 16H208a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h288a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0-320H208a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h288a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16zm0 160H208a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h288a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z"/></svg>';
+
+		const options: IconifyLoaderOptions = {
+			scale: 0,
+		};
+		const result = await getCustomIcon(() => svg, 'a', 'b', options);
+		expect(result && result.indexOf(' width="') === -1).toBeTruthy();
+		expect(result && result.indexOf(' height="') === -1).toBeTruthy();
 	});
 });
