a03d6012041c74d1da058f99ffdbfecb7e65d0fd
Vjacheslav Trushkin <cyberalien@gmail.com>
2023-04-17 21:38:44 +0300

    fix(svelte): add props for svg to component

--- components/svelte/src/svelte.d.ts
+++ components/svelte/src/svelte.d.ts
@@ -1,6 +1,9 @@
 import { SvelteComponentTyped } from 'svelte';
+import { HTMLAttributes } from 'svelte/elements';
 
 /**
  * Svelte component
  */
-export default class Icon extends SvelteComponentTyped<IconProps> {}
+export default class Icon extends SvelteComponentTyped<
+	IconProps & HTMLAttributes<SVGSVGElement>
+> {}
