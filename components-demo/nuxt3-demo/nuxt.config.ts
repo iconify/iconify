fecba21531000c5bfc03c0b06c2658bce2115403
Vjacheslav Trushkin <cyberalien@gmail.com>
2024-11-01 18:32:32 +0200

    chore(vue): update dependencies in component and demo

--- components-demo/nuxt3-demo/nuxt.config.ts
+++ components-demo/nuxt3-demo/nuxt.config.ts
@@ -1,4 +1,7 @@
-import { defineNuxtConfig } from 'nuxt';
-
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
-export default defineNuxtConfig({});
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	future: {
+		compatibilityVersion: 4,
+	},
+	compatibilityDate: '2024-07-16',
+});
