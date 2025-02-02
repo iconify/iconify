812fdc1d6f8a0c9c23bb22d478af223caf50a580
Vjacheslav Trushkin <cyberalien@gmail.com>
2025-02-02 09:10:41 +0200

    chore: fix eslint config in tailwind plugin

--- plugins/tailwind/eslint.config.mjs
+++ plugins/tailwind/eslint.config.mjs
@@ -1,54 +1,58 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/lib", "**/dist"],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended",
-), {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
+export default [
+	{
+		ignores: ['**/lib', '**/dist', '*.mjs'],
+	},
+	...compat.extends(
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:prettier/recommended'
+	),
+	{
+		plugins: {
+			'@typescript-eslint': typescriptEslint,
+		},
 
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-            Atomics: "readonly",
-            SharedArrayBuffer: "readonly",
-        },
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+				Atomics: 'readonly',
+				SharedArrayBuffer: 'readonly',
+			},
 
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "commonjs",
+			parser: tsParser,
+			ecmaVersion: 5,
+			sourceType: 'commonjs',
 
-        parserOptions: {
-            tsconfigRootDir: "/Users/slava/Documents/dev/iconify/iconify/iconify-dev/plugins/tailwind",
-            project: ["tsconfig.json", "tests/tsconfig.json"],
-            extraFileExtensions: [".cjs"],
-        },
-    },
+			parserOptions: {
+				tsconfigRootDir: '.',
+				project: ['tsconfig.json', 'tests/tsconfig.json'],
+			},
+		},
 
-    rules: {
-        "no-mixed-spaces-and-tabs": ["off"],
-        "no-unused-vars": ["off"],
-    },
-}, {
-    files: ["src/**/*.ts", "tests/*.ts"],
-}];
+		rules: {
+			'no-mixed-spaces-and-tabs': ['off'],
+			'no-unused-vars': ['off'],
+		},
+	},
+	{
+		files: ['src/**/*.ts', 'tests/*.ts'],
+	},
+];
