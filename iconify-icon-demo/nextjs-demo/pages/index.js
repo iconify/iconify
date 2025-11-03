383172c8e94fbd0fb560a9214cc706adc3972d81
Vjacheslav Trushkin <cyberalien@gmail.com>
2025-11-03 09:54:48 +0200

    chore: update nextjs to 16

--- iconify-icon-demo/nextjs-demo/pages/index.js
+++ iconify-icon-demo/nextjs-demo/pages/index.js
@@ -30,62 +30,6 @@ export default function Home() {
 			</Head>
 
 			<main>
-				<h1 className="title">
-					Welcome to <a href="https://nextjs.org">Next.js!</a>
-				</h1>
-
-				<p className="description">
-					Get started by editing <code>pages/index.js</code>
-				</p>
-
-				<div className="grid">
-					<a href="https://nextjs.org/docs" className="card">
-						<h3>
-							Documentation <Icon icon="mdi:arrow-right" inline />
-						</h3>
-						<p>
-							Find in-depth information about Next.js features and
-							API.
-						</p>
-					</a>
-
-					<a href="https://nextjs.org/learn" className="card">
-						<h3>
-							Learn <Icon icon="mdi:arrow-right" inline />
-						</h3>
-						<p>
-							Learn about Next.js in an interactive course with
-							quizzes!
-						</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/master/examples"
-						className="card"
-					>
-						<h3>
-							Examples <Icon icon="mdi:arrow-right" inline />
-						</h3>
-						<p>
-							Discover and deploy boilerplate example Next.js
-							projects.
-						</p>
-					</a>
-
-					<a
-						href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className="card"
-					>
-						<h3>
-							Deploy <Icon icon="mdi:arrow-right" inline />
-						</h3>
-						<p>
-							Instantly deploy your Next.js site to a public URL
-							with Vercel.
-						</p>
-					</a>
-				</div>
-
 				<section className="demo">
 					<h1>Iconify demo</h1>
 					<p>
@@ -110,16 +54,6 @@ export default function Home() {
 					</p>
 				</section>
 			</main>
-
-			<footer>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by <Icon icon="logo" alt="Vercel Logo" />
-				</a>
-			</footer>
 
 			<style jsx>{`
 				.container {
