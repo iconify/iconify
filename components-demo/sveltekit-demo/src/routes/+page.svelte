1a9d03d76e125b8d6d1fb2bb0c81937a1bb0a1d2
Vjacheslav Trushkin <cyberalien@gmail.com>
2025-11-03 11:48:31 +0200

    chore: update sveltekit demo

--- components-demo/sveltekit-demo/src/routes/+page.svelte
+++ components-demo/sveltekit-demo/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	import Icon from '@iconify/svelte';
 </script>
 
 <svelte:head>
@@ -8,50 +8,20 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<p>Icon tests: <Icon icon="mdi:home" /><Icon icon="mdi-light:alert" /></p>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
+	p {
+		font-size: 16px;
+		line-height: 24px;
 	}
-
-	h1 {
-		width: 100%;
+	p :global(svg) {
+		font-size: 24px;
+		line-height: 24px;
+		vertical-align: -0.125em;
 	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	p:hover {
+		color: red;
 	}
 </style>
