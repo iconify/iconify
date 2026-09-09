7dbd637188685ecb47abde19ccb9bf3f9d368d16
Howard Guo <50100922+toto6038@users.noreply.github.com>
2026-09-10 05:33:37 +0800

    fix: update expected SVG output in Vue Icon component test

--- components/vue/tests/iconify/10-basic-test.ts
+++ components/vue/tests/iconify/10-basic-test.ts
@@ -70,7 +70,7 @@ describe('Creating component', () => {
 		await nextTick();
 
 		expect(wrapper.html().replace(/\s*\n\s*/g, '')).toBe(
-			`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="1em" height="1em" viewBox="0 0 24 24" class="iconify iconify--${prefix}"><path d="M4 19h16v2H4zm5-4h11v2H9zm-5-4h16v2H4zm0-8h16v2H4zm5 4h11v2H9z" fill="currentColor"></path></svg>`
+			`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--${prefix}" width="1em" height="1em" viewBox="0 0 24 24"><path d="M4 19h16v2H4zm5-4h11v2H9zm-5-4h16v2H4zm0-8h16v2H4zm5 4h11v2H9z" fill="currentColor"></path></svg>`
 		);
 	});
 });
